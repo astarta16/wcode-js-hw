40ab947c228119a767af6f3ec5313eefe6dea7af
astarta16 <ngedeshuri2001@gmail.com>
2024-07-29 17:45:34 +0400

    add another solved problem

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 // N1
 
 let fruits = ["apple", "mango", "avocado", "kiwi"];
-
 fruits.splice(fruits.length - 2, 1, "strawberry");
-
 console.log(fruits);
+
+// N2
+let info = "good day";
+
+let extract = info.slice(info.indexOf("day"));
+console.log(extract);
